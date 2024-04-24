7c8113d3dceb6b6e900e729cc850492ee84e3031
Yousef Hesham <yousefhesham1235@gmail.com>
2024-04-24 16:58:28 +0200

    Server edits

--- server/server.py
+++ server/server.py
@@ -4,11 +4,15 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
-@app.route("/hi", methods=["GET", "POST"])
-def fetch_poster():
-    return "HI"
+@app.route("/", methods=["GET", "POST"])
+def recommend():
+    movie_name = request.form["movie_name"]
+    similar_movies = util.recommend(movie_name)
+    print(similar_movies)
+    # return similar_movies
+    return jsonify(similar_movies)
 
 
 if __name__ == "__main__":
-    fetch_poster()
+    util.loading_artifacts()
     app.run(port=5000)
