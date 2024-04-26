1e11ab4c2b2c1ef49cc1d671d2b98c599931095a
Yousef Hesham <yousefhesham1235@gmail.com>
2024-04-26 16:54:33 +0300

    server edits

--- server/server.py
+++ server/server.py
@@ -1,7 +1,9 @@
 import util
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -9,8 +11,13 @@ def recommend():
     movie_name = request.form["movie_name"]
     similar_movies = util.recommend(movie_name)
     print(similar_movies)
-    # return similar_movies
     return jsonify(similar_movies)
+
+
+@app.route("/suggestions", methods=["GET", "POST"])
+def suggestions():
+    query = request.args.get('query')
+    return util.get_movie_names(query)
 
 
 if __name__ == "__main__":
